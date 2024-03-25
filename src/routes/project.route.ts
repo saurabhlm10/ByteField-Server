231feccc120cea4ac4a9021d1ff7a64385d3c8c1
saurabhlm10 <saurabhlm10@gmail.com>
2024-03-25 16:51:18 +0530

    fix: added verifyToken middleware to project routes

--- src/routes/project.route.ts
+++ src/routes/project.route.ts
@@ -7,12 +7,13 @@ import {
   updateProject,
   deleteProject,
 } from "../controllers/project.controller";
+import { verifyToken } from "../middleware/verify-token.middleware";
 
 const projectRouter = Router();
 
-projectRouter.post("/", asyncHandler(createProject));
-projectRouter.get("/:id", asyncHandler(getProject));
-projectRouter.get("/", asyncHandler(getProjects));
-projectRouter.put("/:id", asyncHandler(updateProject));
-projectRouter.delete("/:id", asyncHandler(deleteProject));
+projectRouter.post("/", verifyToken, asyncHandler(createProject));
+projectRouter.get("/:id", verifyToken, asyncHandler(getProject));
+projectRouter.get("/", verifyToken, asyncHandler(getProjects));
+projectRouter.put("/:id", verifyToken, asyncHandler(updateProject));
+projectRouter.delete("/:id", verifyToken, asyncHandler(deleteProject));
 export default projectRouter;
